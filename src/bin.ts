cd60bd102854d23881e8bbdbc9f7db4e5fd47940
Philippe Beaudoin <philippe.beaudoin@gmail.com>
2024-01-01 15:09:04 -0500

    December 18

--- src/bin.ts
+++ src/bin.ts
@@ -1,64 +1,32 @@
 import minimist from 'minimist'
 import fs from 'fs-extra'
+import { findLargestDay } from './files'
 
 type Func = (lines: string[]) => Promise<any>
-
-import dec1 from './dec1'
-import dec2 from './dec2'
-import dec3 from './dec3'
-import dec4 from './dec4'
-import dec5 from './dec5'
-import dec6 from './dec6'
-import dec7 from './dec7'
-import dec8 from './dec8'
-import dec9 from './dec9'
-import dec10 from './dec10'
-import dec11 from './dec11'
-import dec12 from './dec12'
-import dec13 from './dec13'
-import dec14 from './dec14'
-import dec15 from './dec15'
-import dec16 from './dec16'
-import dec17 from './dec17'
-const days: Func[][] = [
-  dec1,
-  dec2,
-  dec3,
-  dec4,
-  dec5,
-  dec6,
-  dec7,
-  dec8,
-  dec9,
-  dec10,
-  dec11,
-  dec12,
-  dec13,
-  dec14,
-  dec15,
-  dec16,
-  dec17,
-]
 
 const argv = minimist(process.argv.slice(2))
 const day = parseInt(argv._[0], 10)
 const part = argv['1'] ? 1 : argv['2'] ? 2 : 0
-if (isNaN(day) || day < 1 || day > days.length || part === 0) {
-  console.error('Usage: pnpm start DAY [-1 | -2] [--ex]')
-  console.error(`DAY must be between 1 and ${days.length}`)
-  process.exit(1)
-}
-
 const file = argv.ex ? `example` : `input`
 
 async function run() {
+  const maxDay = await findLargestDay()
+
+  if (isNaN(day) || day < 1 || day > maxDay || part === 0) {
+    console.error('Usage: pnpm start DAY [-1 | -2] [--ex]')
+    console.error(`DAY must be between 1 and ${maxDay}`)
+    process.exit(1)
+  }
+
+  const parts = (await import(`./dec${day}`)).default as [Func, Func]
   let path = `./src/dec${day}/${file}`
   if (await fs.pathExists(path + `${part}.txt`)) path += `${part}.txt`
   else path += `.txt`
   const input = await fs.readFile(path, 'utf8')
   const lines = input.split('\n')
   if (lines.length > 0 && lines[lines.length - 1] === '') lines.pop()
-  console.log(`Result: `, await days[day - 1][part - 1](lines))
+
+  console.log(`Result: `, await parts[part - 1](lines))
 }
 
 run()
--- src/dir.ts
+++ src/dir.ts
@@ -3,6 +3,8 @@ import { Pos } from './pos'
 export const DIRS = ['n', 'e', 's', 'w'] as const
 
 export type Dir = (typeof DIRS)[number]
+export type Rotation = 'cw' | 'ccw'
+export type Transformation = Rotation | 'flip' | 'none'
 
 export function flipDir(dir: Dir): Dir {
   switch (dir) {
@@ -15,6 +17,10 @@ export function flipDir(dir: Dir): Dir {
     case 'w':
       return 'e'
   }
+}
+
+export function isFlipDir(dir1: Dir, dir2: Dir): boolean {
+  return flipDir(dir1) === dir2
 }
 
 export function rotateCW(dir: Dir): Dir {
@@ -30,6 +36,10 @@ export function rotateCW(dir: Dir): Dir {
   }
 }
 
+export function isCW(dir1: Dir, dir2: Dir): boolean {
+  return rotateCW(dir1) === dir2
+}
+
 export function rotateCCW(dir: Dir): Dir {
   switch (dir) {
     case 'n':
@@ -43,6 +53,36 @@ export function rotateCCW(dir: Dir): Dir {
   }
 }
 
+export function isCCW(dir1: Dir, dir2: Dir): boolean {
+  return rotateCCW(dir1) === dir2
+}
+
+export function transform(dir: Dir, transformation: Transformation): Dir {
+  switch (transformation) {
+    case 'cw':
+      return rotateCW(dir)
+    case 'ccw':
+      return rotateCCW(dir)
+    case 'flip':
+      return flipDir(dir)
+    case 'none':
+      return dir
+  }
+}
+
+export function getTransformation(dir1: Dir, dir2: Dir): Transformation {
+  if (isCW(dir1, dir2)) return 'cw'
+  if (isCCW(dir1, dir2)) return 'ccw'
+  if (isFlipDir(dir1, dir2)) return 'flip'
+  return 'none'
+}
+
+export function getRotation(dir1: Dir, dir2: Dir): Rotation {
+  if (isCW(dir1, dir2)) return 'cw'
+  if (isCCW(dir1, dir2)) return 'ccw'
+  throw new Error(`Cannot rotate ${dir1} to ${dir2}`)
+}
+
 export function move(pos: Pos, dir: Dir, dist = 1): Pos {
   switch (dir) {
     case 'n':
@@ -53,5 +93,33 @@ export function move(pos: Pos, dir: Dir, dist = 1): Pos {
       return { x: pos.x, y: pos.y + dist }
     case 'w':
       return { x: pos.x - dist, y: pos.y }
+  }
+}
+
+export type URDL = 'U' | 'R' | 'D' | 'L'
+
+export function toCompass(urdl: URDL): Dir {
+  switch (urdl) {
+    case 'U':
+      return 'n'
+    case 'R':
+      return 'e'
+    case 'D':
+      return 's'
+    case 'L':
+      return 'w'
+  }
+}
+
+export function fromCompass(dir: Dir): URDL {
+  switch (dir) {
+    case 'n':
+      return 'U'
+    case 'e':
+      return 'R'
+    case 's':
+      return 'D'
+    case 'w':
+      return 'L'
   }
 }
